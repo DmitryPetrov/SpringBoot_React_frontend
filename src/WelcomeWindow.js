4b8019152ea566d7aa85a005c3b13d31019659a0
Dmitry Petrov <dmitrypetrov93gmail.com>
2019-07-02 13:50:32 +0300

    Added input form handler

--- src/WelcomeWindow.js
+++ src/WelcomeWindow.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import NameInputField from './NameInputField.js'
-import SubmitButton from './SubmitButton.js'
 
 class WelcomeWindow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.textFieldHandler = this.textFieldHandler.bind(this);
+    this.buttonHandler = this.buttonHandler.bind(this);
+  }
+
+  textFieldHandler(event) {
+    this.setState({value: event.target.value});
+  }
+
+  buttonHandler(event) {
+    console.log(this.state.value);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="WelcomeWindow">
-        <form action="index.html" method="post">
-          <NameInputField />
-          <SubmitButton />
+        <form action="index.html" method="post" onSubmit={this.buttonHandler}>
+          <label>User Name:</label>
+          <input type="text" value={this.state.value} onChange={this.textFieldHandler}/>
+
+          <input type="submit" value="Отправить" />
         </form>
       </div>
     )
